d2247fdd671dfd816d55d9e543dc5828c5b27fc7
danielufeli <danielufeli@yahoo.com>
2021-04-09 21:47:17 +0100

    Add Destroy Comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,7 @@ class CommentsController < ApplicationController
     @comment = @article.comments.create(comment_params)
     redirect_to article_path(@article)
   end
-  
+
   def destroy
     @article = Article.find(params[:article_id])
     @comment = @article.comments.find(params[:id])
@@ -13,8 +13,8 @@ class CommentsController < ApplicationController
   end
 
   private
-    def comment_params
-      params.require(:comment).permit(:commenter, :body, :status)
-    end
-end
 
+  def comment_params
+    params.require(:comment).permit(:commenter, :body, :status)
+  end
+end
